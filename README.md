9ba28ea35236070bb926cf3b38b84f62a6555ad6
Lucas Eduardo Campolino <campolino.l@gmail.com>
2022-08-16 04:10:38 -0300

    substring is working fine

---
+++ README.md
@@ -0,0 +1,7 @@
+# substring
+
+this program can count how many times each word, from a dictionary, ocurre in a text
+
+# how to use
+
+create an object of Sub_String, than call the method substring, with a text and a dictionary of class Array
--- sub_string.rb
+++ sub_string.rb
@@ -1,14 +1,17 @@
 class SubString
   def substring(text, dictionary)
-    text.split(' ').each do |string|
+    text.split(' ').reduce(Hash.new(0)) do |result, string|
       dictionary.each do |word| 
-        p word.include?(string)
+        result[word] += 1 if string.match?(word)
       end
+      result
     end
   end
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
+text = "Howdy partner, sit down! How's it going?"
+
 substring = SubString.new
-substring.substring("below", dictionary)
+p substring.substring(text, dictionary)
